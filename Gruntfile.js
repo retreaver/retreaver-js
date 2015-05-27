ab8d10bac1fdf65881d993c50e354f05b6f167f5
Blake Hilscher <blake@hilscher.ca>
2015-05-27 17:26:17 -0400

    include callpixels in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,48 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: [
-        'src/retreaver/base/helpers.js',
-        'src/retreaver/base/cookies.js',
-        'src/retreaver/base/base64.js',
-        'src/retreaver/base/data.js',
-        'src/retreaver/base/model.js',
-        'src/retreaver/base/request.js',
-        'src/retreaver/base/request_number.js',
-        'src/retreaver/cache.js',
-        'src/retreaver/number.js',
-        'src/retreaver/campaign.js'
-        ],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [
+                    'src/retreaver/base/helpers.js',
+                    'src/retreaver/base/cookies.js',
+                    'src/retreaver/base/base64.js',
+                    'src/retreaver/base/data.js',
+                    'src/retreaver/base/model.js',
+                    'src/retreaver/base/request.js',
+                    'src/retreaver/base/request_number.js',
+                    'src/retreaver/cache.js',
+                    'src/retreaver/number.js',
+                    'src/retreaver/campaign.js',
+                    'src/retreaver/callpixels.js'
+                ],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
         }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['concat','uglify']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
