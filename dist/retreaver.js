8fe1a4378f4ecd0adc020dd467d66dd724e31e5a
Blake Hilscher <blake@hilscher.ca>
2015-07-25 14:52:34 -0400

    recompile

--- dist/retreaver.js
+++ dist/retreaver.js
@@ -1,4 +1,32 @@
-(function () {
+(function (context) {
+    /**
+     * @namespace Retreaver
+     */
+    var Retreaver = {
+
+        /**
+         * Configure the retreaver client library.
+         * @function configure
+         * @memberof Retreaver
+         * @param {Object} options
+         * @param {String} options.host - Retreaver API Host
+         * @example
+         * Retreaver.configure({host: 'api.retreaver.com'});
+         *
+         */
+        configure: function (options) {
+            var params = {
+                addr: options.host,
+                http_prefix: 'http',
+                urlregex: "/\\/\\/[^\\/]*\\/(.*)/"
+            };
+            window.Retreaver._connection = new Retreaver.Base.Request(params);
+        }
+    };
+    context.Retreaver = Retreaver;
+
+})(window);
+;(function () {
     // ensure namespace is present
     if (typeof window.Retreaver === 'undefined') window.Retreaver = {};
     var Base = {};
